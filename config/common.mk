bb78fc77887de143298e0f7587df852e29c5f479
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-10-11 22:13:53 +0530

    vendor: Use Pixel boot animation
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -20,6 +20,9 @@ $(call inherit-product, vendor/aosp/config/versioning.mk)
 # Inherit from our kernel/header generator
 $(call inherit-product, vendor/aosp/config/BoardConfigAosp.mk)
 
+# Inherit boot animation
+$(call inherit-product, vendor/aosp/config/bootanimation.mk)
+
 # ThemePicker
 PRODUCT_PACKAGES += \
     ThemePicker
