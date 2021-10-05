020786113b0adb857902b3b8b29c0499d4967f7d
ItsVixano <vixanonew@gmail.com>
2021-10-05 13:04:01 +0200

    [DNM]: build: Temp hax for qcom niggas like me i love you maitreya
    Change-Id: I72990ed2315f4644bad868c90fde00952bc7aff0

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -8,12 +8,22 @@ $(if $(USE_DEVICE_SPECIFIC_$(1)), \
 $(call project-set-path,qcom-$(2),$(strip $(path)))
 endef
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 BOARD_USES_QTI_HARDWARE := true
 
-$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
-$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
-$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
+pathmap_PROJ := \
+    qcom-audio:hardware/qcom-caf/msm8996/audio \
+    qcom-media:hardware/qcom-caf/msm8996/media \
+    qcom-wlan:hardware/qcom-caf/wlan \
+    qcom-display:hardware/qcom-caf/msm8996/display \
+    qcom-bt-vendor:hardware/qcom-caf/bt \
+    qcom-data-ipa-cfg-mgr:vendor/qcom/opensource/data-ipa-cfg-mgr \
+    qcom-dataservices:vendor/qcom/opensource/dataservices \
+    qcom-thermal-hardware/qcom-caf/thermal \
+    qcom-vr:hardware/qcom-caf/vr
+
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/msm8996/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/msm8996/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/msm8996/media)
 
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
@@ -25,4 +35,32 @@ $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
-endif
+#
+# Returns the path to the requested module's include directory,
+# relative to the root of the source tree.  Does not handle external
+# modules.
+#
+# $(1): a list of modules (or other named entities) to find the includes for
+#
+define include-path-for
+$(foreach n,$(1),$(patsubst $(n):%,%,$(filter $(n):%,$(pathmap_INCL))))
+endef
+
+# Enter project path into pathmap
+#
+# $(1): name
+# $(2): path
+#
+define project-set-path
+$(eval pathmap_PROJ += $(1):$(2))
+endef
+
+# Returns the path to the requested module's include directory,
+# relative to the root of the source tree.
+#
+# $(1): a list of modules (or other named entities) to find the projects for
+define project-path-for
+$(foreach n,$(1),$(patsubst $(n):%,%,$(filter $(n):%,$(pathmap_PROJ))))
+endef
+
+include vendor/aosp/build/core/utils.mk
--- config/BoardConfigAosp.mk
+++ config/BoardConfigAosp.mk
@@ -2,9 +2,7 @@
 include vendor/aosp/config/BoardConfigKernel.mk
 
 # Qcom-specific bits
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/aosp/config/BoardConfigQcom.mk
-endif
 
 # Soong
 include vendor/aosp/config/BoardConfigSoong.mk
